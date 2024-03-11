91b8bee93b025ade78bc9a3074010d1daf8d6b75
Victory-soo <victory-soo@naver.com>
2024-03-11 22:06:44 +0900

    fix: I CAN DO

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello ss");
         System.out.println("Git 아직은 어렵다,,,,,");
+        System.out.println("나는 할 수 있다.");
     }
 }
