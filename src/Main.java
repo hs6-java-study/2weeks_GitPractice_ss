abbc493d83f90a245c3c11f1ae2c70d4b76b3cbb
Victory-soo <victory-soo@naver.com>
2024-03-11 22:11:05 +0900

    fix: test

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,6 @@ public class Main {
         System.out.println("Hello ss");
         System.out.println("Git 아직은 어렵다,,,,,");
         System.out.println("나는 할 수 있다.");
+        System.out.println("효성.. 할 수 있다.");
     }
 }
